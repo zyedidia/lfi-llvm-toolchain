05bf23b599351bcc768802fc5ba286cfe1c0b672
Zachary Yedidia <zyedidia@gmail.com>
2024-11-20 23:58:02 -0800

    Change download version

--- download.sh
+++ download.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-git clone https://github.com/zyedidia/llvm-project --depth 1 -b lfi
+git clone https://github.com/zyedidia/llvm-project --depth 1 -b lfi-19.1.4
 git clone https://github.com/zyedidia/musl -b lfi-1.2.5
