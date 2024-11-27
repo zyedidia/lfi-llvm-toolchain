1d238d2dc612ef50b428bff15138b156e689bc25
Zachary Yedidia <zyedidia@gmail.com>
2024-11-27 13:46:10 -0800

    Enable llvm assertions

--- build-llvm.sh
+++ build-llvm.sh
@@ -22,6 +22,7 @@ cmake -G Ninja ../llvm-project/llvm \
     -DLLVM_ENABLE_PROJECTS="lld;clang" \
     -DLLVM_TARGETS_TO_BUILD="X86;AArch64;WebAssembly" \
     -DLLVM_DEFAULT_TARGET_TRIPLE="$ARCH-linux-musl" \
+    -DLLVM_ENABLE_ASSERTIONS=ON \
     -DCMAKE_CXX_COMPILER_LAUNCHER=ccache \
     -DCMAKE_C_COMPILER_LAUNCHER=ccache \
     -DLLVM_BINUTILS_INCDIR=/usr/include \
@@ -53,6 +54,7 @@ cmake -G Ninja ../llvm-project/llvm \
     -DLLVM_BINUTILS_INCDIR=/usr/include \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
     -DLLVM_BUILD_DOCS=OFF \
+    -DLLVM_ENABLE_ASSERTIONS=ON \
     -DLLVM_INSTALL_TOOLCHAIN_ONLY=ON \
     -DCLANG_DEFAULT_RTLIB="compiler-rt" \
     -DCLANG_DEFAULT_CXX_STDLIB="libc++" \
