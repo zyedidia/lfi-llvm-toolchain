fc38f66f05be2726c34017a939a13c2d833c6bf0
Zachary Yedidia <zyedidia@gmail.com>
2024-12-13 15:15:29 -0800

    Copy lfi tools into build bin

--- build-lfi.sh
+++ build-lfi.sh
@@ -15,3 +15,12 @@ export ARCH=$2-lfi
 ./build-libcxx.sh $PREFIX
 
 cp $ARCH-linux-musl.cfg $PREFIX/bin
+
+cp $(which lfi-leg) $PREFIX/lfi-bin
+cp $(which lfi-postlink) $PREFIX/lfi-bin
+cp $(which lfi-verify) $PREFIX/lfi-bin
+
+if [ `uname -m` = "$MARCH" ];
+then
+    cp $(which lfi-run) $PREFIX/lfi-bin
+fi
